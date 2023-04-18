b52a97a313eacad46908b212ea70c84d93a8ff29
tracer-nju <tracer@nju.edu.cn>
2023-04-18 21:36:07 +0800

    Linux dwj 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:36:07 up 2 days,  3:28,  0 users,  load average: 1.08, 1.03, 1.01

--- kernel/include/os.h
+++ kernel/include/os.h
@@ -1,13 +1,29 @@
+#ifndef OS_H__
+#define OS_H__ 
 #include <common.h>
 
 struct task {
-  // TODO
+  struct task *next;
+  const char *name;
+  void   (*entry)(void *);
+  Context  *context;
+  uint8_t *stack;
+  enum {
+    READY,                            
+    RUNNING,
+    NEVER_SCHEDUlE,
+  }status;
 };
 
 struct spinlock {
-  // TODO
+  const char *name;
+  enum{
+    LOCK,
+    UNLOCK,
+  }lock;
 };
 
 struct semaphore {
   // TODO
 };
+#endif
--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -1,4 +1,5 @@
 #include <os.h>
+#define STACK_SIZE 1024 * 8
 
 void kmt_init() {
     int* p = (int*)pmm->alloc(4);
@@ -6,6 +7,31 @@ void kmt_init() {
     printf("%d\n", *p);
 }
 
+int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg) {
+    panic_on(task == NULL, "kmt create error!");
+    task->name = name;
+    task->entry = entry;
+    task->stack = pmm->alloc(STACK_SIZE);
+    task->context = kcontext((Area){task->stack, task->stack + STACK_SIZE}, entry, arg);
+    return 0;
+}
+
+void kmt_teardown(task_t *task) {
+    panic_on(task->status != NEVER_SCHEDUlE, "kmt teardown error!");
+    pmm->free(task->stack);
+}
+
+void kmt_spin_init(spinlock_t *lk, const char *name) {
+    panic_on(lk == NULL, "kmt spin init error!");
+    lk->name = name;
+    lk->lock = UNLOCK;
+}
+
+
+
 MODULE_DEF(kmt) = {
-    .init = kmt_init
+    .init = kmt_init,
+    .create = kmt_create,
+    .teardown = kmt_teardown,
+    .spin_init = kmt_spin_init,
 };
